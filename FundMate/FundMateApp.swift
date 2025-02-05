2305c85d8a7c67eee0f3cb8940cde999c5c3d043
0xmihirsahu <0xmihirsahu@gmail.com>
2025-02-05 10:36:05 +0530

    Initial Commit

---
+++ FundMate/FundMateApp.swift
@@ -0,0 +1,17 @@
+//
+//  FundMateApp.swift
+//  FundMate
+//
+//  Created by Mihir Sahu on 5/2/25.
+//
+
+import SwiftUI
+
+@main
+struct FundMateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FundMateTests/FundMateTests.swift
@@ -0,0 +1,17 @@
+//
+//  FundMateTests.swift
+//  FundMateTests
+//
+//  Created by Mihir Sahu on 5/2/25.
+//
+
+import Testing
+@testable import FundMate
+
+struct FundMateTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
